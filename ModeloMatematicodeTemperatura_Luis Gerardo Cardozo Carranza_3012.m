aad87fa1f0b9fb8a1ef957e9b6751c3ac1757ada
luiscardozo161002 <78442724+luiscardozo161002@users.noreply.github.com>
2021-04-15 20:46:36 -0500

    Update ModeloMatematicodeTemperatura_Luis Gerardo Cardozo Carranza_3012.m

--- ModeloMatematicodeTemperatura_Luis Gerardo Cardozo Carranza_3012.m
+++ ModeloMatematicodeTemperatura_Luis Gerardo Cardozo Carranza_3012.m
@@ -3,32 +3,32 @@
 % Description		:Script para graficar una funcion del cambio de temperatura en grados °F y °C de la forma f(x)=ax+b   
 % Author		:Luis Gerardo Cardozo Carranza (Gerard_CRS) luiscardozo161002@gmail.com
 % Date			:20210415
-% sion		  :1
+% sion		        :1
 % Usage			:octave> /path/ModeloMatematicodeTemperatura_Luis Gerardo Cardozo Carranza_3012
 % Notes			:Requiere aplicacion octave usar en consola preferentemente
 
-%      f(x) = ax + b
-% si se graficá su comportamiento es lineal
+%Modelo_Matematico_de_la_Temperatura_C(f) = ((f*5)/9)+32
+
+% Si se graficá su comportamiento es lineal
 %      ^      
-%    y |     /
-%      |    /
+%    y |    /
 %      |   /
 %      |  /
+%      | /
 %      |/_____________  > x
 %
 
-% limpiar variables
- clear
-% Ejemplo f(x) = a
-% Rango de -0..40 en i = 10
- x=-100:10:100;
-% Valor de la funcion 
- y=-212:53:212; 
-% Dibujar x,y
- plot(x,y);
-% Titulo
- title(" f(x) = ax + b");
-% Etiqueta para x
- xlabel("x");
-% Etiqueta para y
- ylabel("f(x)");
+%limpiar variables
+clear
+%rango de -212 .. 212 en i = 0.2
+f=-212:0.2:212;
+%valor de la funcion
+C=((f*5)/9)+32;
+%dibujar x,y 
+plot (f,C);
+%titulo
+title("Modelo Matematico de la Temperatura C   (f) =((f*5)/9)+32");
+%etiqueta para x
+xlabel ("Fahrenheit(F)");
+%etiqueta para y
+ylabel ("Celcius(C)");
