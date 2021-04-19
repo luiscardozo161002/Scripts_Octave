5a7c8ae37a194bc44012e61820e4c01a202b9e22
luiscardozo161002 <78442724+luiscardozo161002@users.noreply.github.com>
2021-04-19 07:55:32 -0500

    Create DominiodeFuncionesF2LuisgGerardoCardozoCarranza3202.m

---
+++ DominiodeFuncionesF2LuisgGerardoCardozoCarranza3202.m
@@ -0,0 +1,25 @@
+% Octave Script
+% Title			:ModelosMatematicos_Lamina 16
+% Description		:Script para graficar el ejericio 2 de la lamina 16.
+% Author		                :Luis Gerardo Cardozo Carranza (Gerard_CRS) luiscardozo161002@gmail.com
+% Date			:20210415
+% sion		                :1
+% Usage			:octave> /path/ModeloMatematicos_Lamina_15_Luis Gerardo Cardozo Carranza_3012
+% Notes			:Requiere aplicacion octave usar en consola preferentemente
+
+%Lamina 16_Ejercicio 1 f1(x)=x-1/x^5x+6
+% Si se graficá su comportamiento es radical
+%Limpiar variables.
+clear
+%Dominio de la funcion.
+x=10:0.2:10;
+%Valor de la funcion.
+y=(x(-1)/((x)^5(x))+6);
+%Genera la grafica 
+plot(x,y,'b');
+%Asigna un nombre a la grafica 
+title("Modelo matematico Lamina 16 Ejericicio 1 f1(x)=4x/v2x-1");
+%Etiqueta para x
+xlabel("x");
+%Etiqueta para y
+ylabel("y");
